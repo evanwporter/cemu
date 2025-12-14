4010468e27e82c4bac9af73dada163dd96b04001
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2025-12-13 17:19:15 -0800

    changes

--- vemu/src/mmu/DMA.sv
+++ vemu/src/mmu/DMA.sv
@@ -14,15 +14,17 @@ module DMA (
   // DMA
   logic [7:0] DMA;
   logic dma_active;
-  logic [7:0] dma_index;  // sprite index 0-40
+  logic [7:0] dma_index;  // byte index for OAM DMA (0-159)
 
   wire dma_selected = bus.addr == DMA_OAM_addr;
 
-  wire [15:0] DMA_read_addr = {DMA, dma_index * 8'd4};
+  wire [15:0] DMA_read_addr = {DMA, dma_index};
   wire [15:0] DMA_write_addr = {OAM_start[15:8], dma_index};
 
+  // Write logic
   always_ff @(posedge clk or posedge reset) begin
     if (reset) begin
+      `LOG_INFO(("[MMU] [DMA] DMA reset"));
       DMA <= 8'd0;
       dma_index <= 8'd0;
       dma_active <= 1'b0;
@@ -30,12 +32,27 @@ module DMA (
       DMA <= bus.wdata;
       dma_index <= 8'd0;
       dma_active <= 1'b1;
+      `LOG_INFO(("[MMU] [DMA] DMA started source=%02h00", bus.wdata));
     end
   end
 
+  // Read logic
   always_comb begin
     bus.rdata = 8'hFF;
     if (bus.read_en && dma_selected) bus.rdata = DMA;
+  end
+
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      dma_index <= 8'd0;
+    end else if (dma_active) begin
+      if (dma_index == 8'd159) begin
+        dma_active <= 1'b0;
+        dma_index  <= 8'd0;
+      end else begin
+        dma_index <= dma_index + 8'd1;
+      end
+    end
   end
 
 endmodule
