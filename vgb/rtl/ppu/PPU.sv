421a3e7cd9d3482c988dbfd3864f3a3775e69b5a
Evan Porter <115374841+evanwporter@users.noreply.github.com>
2026-02-10 21:05:23 -0800

    rename project
    update GBA test submodule

---
+++ vgb/rtl/ppu/PPU.sv
@@ -0,0 +1,442 @@
+import ppu_types_pkg::*;
+import mmu_addresses_pkg::*;
+import ppu_util_pkg::*;
+
+`include "util/logger.svh"
+
+module PPU (
+    input logic clk,
+    input logic reset,
+    Bus_if.Slave_side bus,
+    Interrupt_if.PPU_side IF_bus,
+    DMA_if.DMA_side dma_bus
+);
+
+  // Dot counters (resets after each line)
+  logic [8:0] dot_counter;
+
+  ppu_regs_t regs;
+
+  ppu_mode_t mode;
+
+  logic flush;
+
+  logic [7:0] VRAM[VRAM_len];
+  logic [7:0] OAM[OAM_len];
+
+  wire LCD_enabled = regs.LCDC[7];
+
+  // ======================================================
+  // Renderer Submodules
+  // ======================================================
+
+  // TODO: better name
+  logic pixel_transfer_en;
+  assign pixel_transfer_en = (mode == PPU_MODE_3);
+
+  logic line_done;
+
+  (* maybe_unused *)
+  logic frame_done;
+
+  object_t sprites_found[10];
+
+  Fetcher_if fetcher_bus (
+      .regs(regs),
+      .dot_counter(dot_counter),
+      .mode(mode)
+  );
+
+  Fetcher_if obj_fetcher_bus (
+      .regs(regs),
+      .dot_counter(dot_counter),
+      .mode(mode)
+  );
+
+  FIFO_if fifo_bus ();
+
+  FIFO_if obj_fifo_bus ();
+
+  RenderingControl_if rendering_control_bus ();
+
+  // Fetcher
+  Fetcher fetcher_inst (
+      .clk(clk),
+      .reset(reset),
+      .bus(fetcher_bus),
+      .fifo_bus(fifo_bus),
+      .control_bus(rendering_control_bus),
+      .flush(flush)
+  );
+
+  ObjFetcher obj_fetcher_inst (
+      .clk        (clk),
+      .reset      (reset),
+      .bus        (obj_fetcher_bus),
+      .control_bus(rendering_control_bus),
+      .fifo_bus   (obj_fifo_bus),
+      .sprite_buf (sprites_found)
+  );
+
+  // FIFO
+  FIFO fifo_inst (
+      .clk  (clk),
+      .reset(reset),
+      .bus  (fifo_bus),
+      .flush(flush)
+  );
+
+  // FIFO for objects
+  Obj_FIFO obj_fifo_inst (
+      .clk  (clk),
+      .reset(reset),
+      .bus  (obj_fifo_bus),
+      .flush(flush)
+  );
+
+  // Framebuffer
+  Framebuffer framebuffer_inst (
+      .clk(clk),
+      .reset(reset),
+      .pixel_transfer_en(pixel_transfer_en),
+      .fifo_bus(fifo_bus),
+      .obj_fifo_bus(obj_fifo_bus),
+      .control_bus(rendering_control_bus),
+      .flush(flush),
+      .regs(regs),
+      .line_done(line_done),
+      .frame_done(frame_done)
+  );
+
+
+  // ======================================================
+  // Update STAT
+  // ======================================================
+
+  logic [7:0] LY_prev;
+  ppu_mode_t mode_prev;
+
+  // Enable STAT interrupt requests
+  wire stat_en_lyc = regs.STAT[6];
+  wire stat_en_mode2 = regs.STAT[5];
+  wire stat_en_mode1 = regs.STAT[4];
+  wire stat_en_mode0 = regs.STAT[3];
+
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      IF_bus.stat_req <= 1'b0;
+    end else begin
+      IF_bus.stat_req <= 1'b0;
+
+      if (stat_en_lyc && regs.LY != LY_prev && regs.LY == regs.LYC) begin
+        IF_bus.stat_req <= 1'b1;
+      end else if (mode != mode_prev) begin
+        if (stat_en_mode2 && mode == PPU_MODE_2) begin
+          IF_bus.stat_req <= 1'b1;
+        end else if (stat_en_mode1 && mode == PPU_MODE_1) begin
+          IF_bus.stat_req <= 1'b1;
+        end else if (stat_en_mode0 && mode == PPU_MODE_0) begin
+          IF_bus.stat_req <= 1'b1;
+        end
+      end
+
+      LY_prev   <= regs.LY;
+      mode_prev <= mode;
+    end
+  end
+
+
+  // ======================================================
+  // Address Selections
+  // ======================================================
+  wire VRAM_selected = bus.addr inside {[VRAM_start : VRAM_end]};
+  wire OAM_selected = bus.addr inside {[OAM_start : OAM_end]};
+  wire PPU_regs_selected = bus.addr inside {[PPU_regs_start : PPU_regs_end]} & (bus.addr != DMA_OAM_addr);
+
+
+  // ======================================================
+  // Write
+  // ======================================================
+  always_ff @(posedge clk or posedge reset) begin
+    if (bus.write_en) begin
+      `LOG_TRACE(("PPU: WRITE addr=%h data=%h", bus.addr, bus.wdata))
+
+      // VRAM writes (blocked in Mode 3)
+      if (VRAM_selected) begin
+        if (!LCD_enabled || mode != PPU_MODE_3) begin
+          VRAM[13'(bus.addr-16'h8000)] <= bus.wdata;
+          `LOG_TRACE(("[PPU] VRAM WRITE addr=%h data=%h (mode=%0d)", bus.addr, bus.wdata, mode))
+        end else begin
+          `LOG_TRACE(
+              ("[PPU] VRAM WRITE BLOCKED addr=%h data=%h (mode=%0d)", bus.addr, bus.wdata, mode))
+        end
+      end
+
+      // OAM writes (blocked in Mode 2 & 3)
+      if (OAM_selected) begin
+        if (!LCD_enabled || dma_bus.active || !(mode == PPU_MODE_2 || mode == PPU_MODE_3)) begin
+          OAM[8'(bus.addr-16'hFE00)] <= bus.wdata;
+          `LOG_TRACE(("[PPU] OAM WRITE addr=%h data=%h (mode=%0d)", bus.addr, bus.wdata, mode))
+        end else begin
+          `LOG_INFO(
+              ("[PPU] OAM WRITE BLOCKED addr=%h data=%h (mode=%0d)", bus.addr, bus.wdata, mode))
+        end
+      end
+
+      // PPU register writes
+      // TODO: check whether this ever needs to be blocked 
+      if (PPU_regs_selected) begin
+        `LOG_TRACE(("[PPU] REG WRITE addr=%h data=%h", bus.addr, bus.wdata))
+        case (bus.addr)
+          16'hFF40: regs.LCDC <= bus.wdata;
+          16'hFF41: regs.STAT[6:3] <= bus.wdata[6:3];  // only bits 3-6 are writable
+          16'hFF42: regs.SCY <= bus.wdata;
+          16'hFF43: regs.SCX <= bus.wdata;
+          16'hFF44: begin
+            `LOG_WARN(("[PPU] Attempted to write 0x%h to LY register", bus.wdata))
+          end
+          16'hFF45: regs.LYC <= bus.wdata;
+          // DMA transfer handled elsewhere
+          16'hFF47: regs.BGP <= bus.wdata;
+          16'hFF48: regs.OBP0 <= bus.wdata;
+          16'hFF49: regs.OBP1 <= bus.wdata;
+          16'hFF4A: regs.WY <= bus.wdata;
+          16'hFF4B: regs.WX <= bus.wdata;
+          default:  ;
+        endcase
+      end
+    end
+  end
+
+
+  // ======================================================
+  // Read
+  // ======================================================
+  always_comb begin
+    bus.rdata = 8'hFF;  // open bus unless selected & allowed
+    fetcher_bus.rdata = 8'hFF;
+    obj_fetcher_bus.rdata = 8'hFF;
+
+    if (obj_fetcher_bus.read_req) begin
+      // VRAM reads for obj fetcher (not blocked in Mode 3)
+      obj_fetcher_bus.rdata = VRAM[13'(obj_fetcher_bus.addr-VRAM_start)];
+      // $display("[PPU] VRAM OBJ FETCHER READ addr=%h -> %h (mode=%0d)", obj_fetcher_bus.addr,
+      //          obj_fetcher_bus.rdata, mode);
+    end
+
+    if (fetcher_bus.read_req) begin
+      // VRAM reads for fetcher (not blocked in Mode 3)
+      fetcher_bus.rdata = VRAM[13'(fetcher_bus.addr-VRAM_start)];
+      // $display("[PPU] VRAM FETCHER READ addr=%h -> %h (mode=%0d)", fetcher_bus.addr,
+      //          fetcher_bus.rdata, mode);
+    end
+
+    if (bus.read_en) begin
+
+      // VRAM reads (blocked in Mode 3)
+      if (VRAM_selected) begin
+        bus.rdata = (mode == PPU_MODE_3) ? 8'hFF : VRAM[13'(bus.addr-VRAM_start)];
+        `LOG_TRACE(("[PPU] VRAM READ addr=%h -> %h (mode=%0d)", bus.addr, bus.rdata, mode))
+      end
+
+      // OAM reads (blocked in Mode 2 & 3)
+      if (OAM_selected) begin
+        bus.rdata = dma_bus.active ? 8'hFF : OAM[8'(bus.addr-OAM_start)];
+        `LOG_TRACE(("[PPU] OAM READ addr=%h -> %h (mode=%0d)", bus.addr, bus.rdata, mode))
+      end
+
+      // PPU register reads
+      if (PPU_regs_selected) begin
+        `LOG_TRACE(("[PPU] REG READ addr=%h -> %h", bus.addr, bus.rdata))
+        case (bus.addr)
+          16'hFF40: bus.rdata = regs.LCDC;
+          16'hFF41: bus.rdata = {1'b1, regs.STAT[6:2], mode};
+          16'hFF42: bus.rdata = regs.SCY;
+          16'hFF43: bus.rdata = regs.SCX;
+          16'hFF44: bus.rdata = regs.LY;
+          16'hFF45: bus.rdata = regs.LYC;
+          // 16'hFF46: bus.rdata = regs.DMA;
+          16'hFF47: bus.rdata = regs.BGP;
+          16'hFF48: bus.rdata = regs.OBP0;
+          16'hFF49: bus.rdata = regs.OBP1;
+          16'hFF4A: bus.rdata = regs.WY;
+          16'hFF4B: bus.rdata = regs.WX;
+          default:  bus.rdata = 8'hFF;
+        endcase
+      end
+    end
+  end
+
+
+  // ======================================================
+  // Update Mode
+  // ======================================================
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      mode <= PPU_MODE_2;
+    end else begin
+      flush <= 1'b0;
+
+      if (!regs.LCDC[7]) begin
+        mode <= PPU_MODE_2;
+      end else begin
+        // Visible lines
+        unique case (mode)
+          // OAM Scan
+          PPU_MODE_2: begin
+            // On dot_counter 80, enter Pixel Transfer
+            if (dot_counter == MODE2_LEN - 1) begin
+              mode  <= PPU_MODE_3;
+              flush <= 1'b1;
+              // $display("[PPU] Entering MODE 3 at LY=%0d", regs.LY);
+            end
+          end
+
+          // Pixel Transfer
+          PPU_MODE_3: begin
+            if (line_done) begin
+              mode <= PPU_MODE_0;
+              // $display("[PPU] Entering MODE 0 at LY=%0d", regs.LY);
+            end
+          end
+
+          // HBlank
+          PPU_MODE_0: begin
+            if (dot_counter == DOTS_PER_LINE - 1) begin
+              if (regs.LY == GB_SCREEN_HEIGHT - 1)
+                mode <= PPU_MODE_1;  // enter VBlank after line 143
+              else mode <= PPU_MODE_2;
+            end
+          end
+
+          // VBlank
+          PPU_MODE_1: begin
+            if (regs.LY == LINES_PER_FRAME - 1 && dot_counter == DOTS_PER_LINE - 1)
+              mode <= PPU_MODE_2;  // start next frame
+          end
+        endcase
+      end
+    end
+  end
+
+
+  // ======================================================
+  // Tick
+  // ======================================================
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      dot_counter <= 9'd0;
+      regs.LY <= 8'd0;
+      IF_bus.vblank_req <= 1'b0;
+    end else begin
+      // Regardless of mode, clear VBlank interrupt request
+      IF_bus.vblank_req <= 1'd0;
+
+      regs.STAT[2] <= regs.LY == regs.LYC;
+
+      if (regs.LCDC[7] == 1'b0) begin  // LCD disabled
+        dot_counter <= 9'd0;
+        regs.LY <= 8'd0;
+      end else begin
+
+        // Advance dot
+        if (dot_counter == DOTS_PER_LINE - 1) begin  // Reached end of line
+          dot_counter <= 9'd0;
+
+          // Reset LY at end of frame
+          if (regs.LY == LINES_PER_FRAME - 1) regs.LY <= 8'd0;
+
+          // Advance LY at end of line
+          else
+            regs.LY <= regs.LY + 1'b1;
+
+          // Raise vblank interrupt exactly when entering LY=144
+          if (regs.LY == GB_SCREEN_HEIGHT - 1) IF_bus.vblank_req <= 1'b1;
+
+        end else begin
+          // If we haven't reached end of line, then vgba dot
+          dot_counter <= dot_counter + 1'b1;
+        end
+      end
+    end
+  end
+
+
+  // ======================================================
+  // OAM Scan
+  // ======================================================
+
+  /// Current sprite being scanned (0-39)
+  /// Each sprite takes 2 dots to scan
+  /// Effectively: `dot_counter / 2`
+  wire [5:0] sprite_num = dot_counter[6:1];
+
+  /// Current sprite index in OAM (0-159)
+  /// Effectively: `sprite_num * 4`
+  wire [7:0] sprite_idx = 8'(sprite_num) << 2;
+
+  logic [3:0] sprites_found_count;
+
+  enum logic {
+    T1,
+    T2
+  } scan_state;
+
+  assign scan_state = (dot_counter[0]) ? T2 : T1;
+
+  wire [4:0] sprite_height = regs.LCDC[2] ? 5'd16 : 5'd8;
+
+  object_t current_sprite;
+  always_comb begin
+    current_sprite.y_pos = OAM[sprite_idx+0];
+    current_sprite.x_pos = OAM[sprite_idx+1];
+    current_sprite.tile_idx = OAM[sprite_idx+2];
+    current_sprite.attr = OAM[sprite_idx+3];
+    current_sprite.oam_idx = sprite_num;
+    current_sprite.valid = 1'b1;
+  end
+
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      for (int i = 0; i < 10; i++) begin
+        sprites_found[i].y_pos    <= 8'd0;
+        sprites_found[i].x_pos    <= 8'd0;
+        sprites_found[i].tile_idx <= 8'd0;
+        sprites_found[i].attr     <= 8'd0;
+        sprites_found[i].valid    <= 1'b0;
+      end
+      sprites_found_count <= 4'd0;
+    end else begin
+      if (dot_counter == 9'd455) begin
+        sprites_found_count <= 4'd0;
+        for (int i = 0; i < 10; i++) begin
+          sprites_found[i].valid <= 1'b0;
+        end
+      end
+
+      if (mode == PPU_MODE_2) begin
+        unique case (scan_state)
+          T1: begin
+            if ((sprites_found_count < 10) &&
+                (regs.LY + 16 >= current_sprite.y_pos) &&
+                (regs.LY + 16 <  current_sprite.y_pos + 8'(sprite_height))) begin
+              sprites_found[sprites_found_count] <= current_sprite;
+              sprites_found_count <= sprites_found_count + 1'b1;
+              if (current_sprite.y_pos == 8'd56) begin
+                // $display("[%0d] DC=%0d OAM idx=%0d -> write sprite[%0d] x=%0d y=%0d", sprite_num,
+                //          dot_counter, sprite_idx, sprites_found_count, current_sprite.x_pos,
+                //          current_sprite.y_pos);
+              end
+            end else begin
+              // $display("[%0d] DC=%0d OAM idx=%0d -> skip", sprite_num, dot_counter, sprite_idx);
+            end
+          end
+
+          T2: begin
+            //
+          end
+        endcase
+      end
+    end
+  end
+endmodule
---
+++ vgba/rtl/cpu/CPU.sv
@@ -0,0 +1,315 @@
+import types_pkg::*;
+import cpu_types_pkg::*;
+import control_types_pkg::*;
+import cpu_util_pkg::*;
+
+`include "cpu/util.svh"
+
+module CPU (
+    input logic clk,
+    input logic reset,
+
+    Bus_if.Master_side bus
+);
+
+  /// TODO: Remove
+  control_t control_signals;
+
+  word_t IR;
+
+  logic flush_req;
+  logic flush_req_pending;
+
+  cpu_regs_t regs;
+
+  word_t A_bus;
+  word_t B_bus;
+
+  cpu_mode_t cpu_mode;
+
+  /// Data that has been latched from the read bus
+  word_t read_data;
+
+  /// Address associated with above read data; used for byte loads
+  word_t addr_data;
+
+  always_comb begin
+    casez (regs.CPSR[4:0])
+
+      5'b0??00: cpu_mode = MODE_USR;  // Old User
+      5'b0??01: cpu_mode = MODE_FIQ;  // Old FIQ
+      5'b0??10: cpu_mode = MODE_IRQ;  // Old IRQ
+      5'b0??11: cpu_mode = MODE_SVC;  // Old Supervisor
+
+      5'b10000: cpu_mode = MODE_USR;  // User
+      5'b10001: cpu_mode = MODE_FIQ;  // FIQ
+      5'b10010: cpu_mode = MODE_IRQ;  // IRQ
+      5'b10011: cpu_mode = MODE_SVC;  // Supervisor
+      5'b10111: cpu_mode = MODE_ABT;  // Abort
+      5'b11011: cpu_mode = MODE_UND;  // Undefined
+      5'b11111: cpu_mode = MODE_SYS;  // System
+
+      default: begin
+        cpu_mode = MODE_USR;
+        $warning("Illegal CPSR mode encoding: %b", regs.CPSR[4:0]);
+      end
+    endcase
+  end
+
+  Decoder_if decoder_bus (
+      .IR(IR),
+      .flags(regs.CPSR[31:28])
+  );
+
+  ALU_if alu_bus (.op_a(A_bus));
+  Shifter_if shifter_bus (.R_in(B_bus));
+
+  assign shifter_bus.shift_latch_amt = control_signals.shift_latch_amt;
+  assign shifter_bus.shift_use_latch = control_signals.shift_use_latch;
+  assign shifter_bus.shift_amount = control_signals.shift_amount;
+  assign shifter_bus.shift_type = control_signals.shift_type;
+  assign shifter_bus.carry_in = regs.CPSR[29];  // CPSR.C
+  assign shifter_bus.shift_use_rxx = control_signals.shift_use_rxx;
+
+  assign alu_bus.alu_op = control_signals.ALU_op;
+  assign alu_bus.use_op_b_latch = control_signals.ALU_use_op_b_latch;
+  assign alu_bus.disable_op_b = control_signals.ALU_disable_op_b;
+  assign alu_bus.latch_op_b = control_signals.ALU_latch_op_b;
+  assign alu_bus.flags_in = regs.CPSR[31:28];  // N,Z,C,V
+
+  assign bus.read_en = control_signals.memory_read_en;
+  assign bus.write_en = control_signals.memory_write_en;
+  assign bus.wdata = B_bus;
+
+  /// TODO: Debug signal
+  (* maybe_unused *)
+  logic instr_boundary;
+
+  // assign decoder_bus.IR = IR;
+
+  ALU alu_inst (
+      .clk(clk),
+      .reset(reset),
+      .bus(alu_bus),
+      .shifter_bus(shifter_bus)
+  );
+
+  Decoder decoder_inst (
+      .clk  (clk),
+      .reset(reset),
+      .bus  (decoder_bus)
+  );
+
+  ControlUnit controlUnit (
+      .clk(clk),
+      .reset(reset),
+      .decoder_bus(decoder_bus),
+      .control_signals(control_signals),
+      .flush_req(flush_req)
+  );
+
+  BarrelShifter shifter_inst (
+      .clk  (clk),
+      .reset(reset),
+      .bus  (shifter_bus)
+  );
+
+  // ======================================================
+  // Assign A Bus
+  // ======================================================
+
+  // This may get more complicated in the future
+  assign A_bus = control_signals.pc_rn_add_4 ? (read_reg(
+      regs, cpu_mode, decoder_bus.word.Rn
+  ) + 32'd4) : read_reg(
+      regs, cpu_mode, decoder_bus.word.Rn
+  );
+
+  function automatic word_t ror32(word_t x, int unsigned sh);
+    ror32 = (x >> sh) | (x << (32 - sh));
+  endfunction
+
+  // ======================================================
+  // Assign B Bus
+  // ======================================================
+  always_comb begin
+    B_bus = 32'd0;
+
+    unique case (control_signals.B_bus_source)
+      B_BUS_SRC_NONE: begin
+        B_bus = 32'd0;
+      end
+
+      B_BUS_SRC_IMM: begin
+        B_bus = {20'b0, control_signals.B_bus_imm};
+      end
+
+      B_BUS_SRC_READ_DATA: begin
+        B_bus = read_data;
+        $display("Driving B bus with read_data value: 0x%08x", read_data);
+      end
+
+      B_BUS_SRC_REG_RM: begin
+        B_bus = control_signals.pc_rm_add_4 ? (read_reg(regs, cpu_mode, decoder_bus.word.Rm) + 32'd4
+            ) : read_reg(regs, cpu_mode, decoder_bus.word.Rm);
+      end
+
+      B_BUS_SRC_REG_RS: begin
+        B_bus = control_signals.pc_rs_add_4 ? (read_reg(regs, cpu_mode, decoder_bus.word.Rs) + 32'd4
+            ) : read_reg(regs, cpu_mode, decoder_bus.word.Rs);
+      end
+
+      B_BUS_SRC_REG_RD: begin
+        B_bus = read_reg(regs, cpu_mode, decoder_bus.word.Rd);
+      end
+    endcase
+  end
+
+  always_ff @(posedge clk) begin
+    `DISPLAY_CONTROL(control_signals)
+
+    instr_boundary <= control_signals.pipeline_advance;
+  end
+
+  // ======================================================
+  // Memory Module
+  // ======================================================
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      IR <= 32'd0;
+    end else begin
+      `TRACE_CPU
+
+      assert (!(control_signals.memory_write_en && control_signals.memory_read_en))
+      else $fatal(1, "Both memory_read_en and memory_write_en asserted!");
+
+      if (control_signals.memory_read_en) begin
+        if (control_signals.memory_latch_IR) begin
+          IR <= bus.rdata;
+          $display("Latching IR with value: 0x%08x", bus.rdata);
+          $fflush();
+
+        end else begin
+          read_data <= bus.rdata;
+
+          // Misaligned word-load rotate quirk (ARM7TDMI)
+          if (!control_signals.memory_byte_transfer) begin
+            logic [1:0] a;
+            a = bus.addr[1:0];
+            if (a != 2'b00) begin
+              $display("Misaligned word with a=%b, rotate=%d, prior=%d", a, ror32(
+                       bus.rdata, 32'({a, 3'b000})), bus.rdata);
+              read_data <= ror32(bus.rdata, 32'({a, 3'b000}));  // (a*8)
+            end
+          end
+
+          $display("ALU is driving address bus with value: 0x%08x", alu_bus.result);
+          addr_data <= alu_bus.result;
+        end
+      end
+    end
+  end
+
+  // ======================================================
+  // Perform Register Writebacks
+  // ======================================================
+  always_ff @(posedge clk) begin
+    if (reset) begin
+      regs.user <= '{default: 32'd0};
+    end else begin
+      flush_req <= 1'b0;
+
+      if (control_signals.pipeline_advance && flush_req_pending) begin
+        $display("Pipeline vgba, checking for writebacks and flushes");
+        $fflush();
+
+        flush_req_pending <= 1'b0;
+        flush_req <= 1'b1;
+      end
+
+      if ((control_signals.ALU_writeback == ALU_WB_REG_RD && decoder_bus.word.Rd == 4'd15) ||
+          (control_signals.ALU_writeback == ALU_WB_REG_RN && decoder_bus.word.Rn == 4'd15)) begin
+
+        if (control_signals.pipeline_advance) begin
+          flush_req <= 1'b1;
+        end else begin
+          flush_req_pending <= 1'b1;
+          $display("Setting flush_req_pending to ensure flush on next cycle.");
+        end
+      end else if (control_signals.incrementer_writeback) begin
+        // PC = PC + 4
+
+        `WRITE_REG(regs, cpu_mode, 15, read_reg(regs, cpu_mode, 15) + 32'd4)
+        $display("Incrementing PC to: %0d", read_reg(regs, cpu_mode, 15) + 32'd4);
+        $fflush();
+      end
+
+      if (control_signals.ALU_set_flags && control_signals.pipeline_advance) begin
+        if ((decoder_bus.word.Rd == 4'd15) && mode_has_spsr(cpu_mode)) begin
+          regs.CPSR <= read_spsr(regs, cpu_mode);
+
+          $display("Restoring CPSR from SPSR_%0d: 0x%08x", cpu_mode, read_spsr(regs, cpu_mode));
+          $fflush();
+        end else begin
+          $display("Setting flags: N=%b, Z=%b, C=%b, V=%b", alu_bus.flags_out.n,
+                   alu_bus.flags_out.z, alu_bus.flags_out.c, alu_bus.flags_out.v);
+
+          regs.CPSR[31] <= alu_bus.flags_out.n;
+          regs.CPSR[30] <= alu_bus.flags_out.z;
+          regs.CPSR[29] <= alu_bus.flags_out.c;
+          regs.CPSR[28] <= alu_bus.flags_out.v;
+
+          $display("ALU op was %0d, setting C flag to %b", control_signals.ALU_op,
+                   alu_bus.flags_out.c);
+          $fflush();
+        end
+      end
+
+      unique case (control_signals.ALU_writeback)
+        ALU_WB_NONE:   ;
+        ALU_WB_REG_RD: begin
+          word_t value_to_write;
+          value_to_write = alu_bus.result;
+          `WRITE_REG(regs, cpu_mode, decoder_bus.word.Rd, value_to_write)
+          $display("Writing back ALU result %0d to Rd (R%d)", value_to_write, decoder_bus.word.Rd);
+        end
+        ALU_WB_REG_RS: `WRITE_REG(regs, cpu_mode, decoder_bus.word.Rs, alu_bus.result)
+        ALU_WB_REG_RN: begin
+          $display("Writing back ALU result %0d to Rn (R%d)", alu_bus.result, decoder_bus.word.Rn);
+          `WRITE_REG(regs, cpu_mode, decoder_bus.word.Rn, alu_bus.result)
+        end
+        ALU_WB_REG_14: `WRITE_REG(regs, cpu_mode, 14, alu_bus.result)
+      endcase
+    end
+  end
+
+  // ======================================================
+  // Address Module
+  // ======================================================
+  // Calculate address bus value
+  always_ff @(posedge clk) begin
+    if (reset) begin
+    end else begin
+      unique case (control_signals.addr_bus_src)
+        ADDR_SRC_NONE: begin
+          bus.addr <= 32'd0;
+        end
+
+        ADDR_SRC_ALU: begin
+          bus.addr <= alu_bus.result;
+        end
+
+        ADDR_SRC_PC: begin
+          // PC
+          $display("Setting address bus to PC value: 0x%08x", read_reg(regs, cpu_mode, 15));
+          bus.addr <= read_reg(regs, cpu_mode, 15);
+        end
+
+        ADDR_SRC_INCR: begin
+          bus.addr <= bus.addr + 32'd4;
+        end
+      endcase
+    end
+  end
+
+endmodule : CPU
